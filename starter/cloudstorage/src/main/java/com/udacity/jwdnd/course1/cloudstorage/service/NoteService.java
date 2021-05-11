db92d242ddbbd58b830967f7548a0cb37ac36d13
sabrine-gamdou <sgamdou@stud.hs-bremen.de>
2021-05-11 21:49:39 +0200

    Created a service to handle the business logic of the notes objects

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/NoteService.java
@@ -0,0 +1,34 @@
+package com.udacity.jwdnd.course1.cloudstorage.service;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    private final NoteMapper noteMapper;
+
+    public NoteService(NoteMapper noteMapper){
+        this.noteMapper = noteMapper;
+    }
+
+    public int createNote(Note note){
+        return noteMapper.insert(new Note(null, note.getNoteTitle(), note.getNoteDescription(), note.getUserId()));
+    }
+
+    public List<Note> getNotes(User user){
+        return noteMapper.getNotes(user.getUserId());
+    }
+
+    public int updateNote(Note note) {
+        return noteMapper.update(new Note(note.getNoteId(), note.getNoteTitle(), note.getNoteDescription(), note.getUserId()));
+    }
+
+    public int deleteNote(int noteId){
+        return noteMapper.delete(noteId);
+    }
+}
