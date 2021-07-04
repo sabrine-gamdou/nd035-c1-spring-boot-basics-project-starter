e11ea1e4b882bff047f35a6bc5f02e2f7e2b03fe
sabrine-gamdou <sgamdou@stud.hs-bremen.de>
2021-07-04 14:52:23 +0200

    Added method to retrieve note via title

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -11,6 +11,9 @@ public interface NoteMapper {
     @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
     List<Note> getNotes(int userId);
 
+    @Select("SELECT * FROM NOTES WHERE notetitle = #{noteTitle}")
+    Note getNote(String noteTitle);
+
     @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
             "VALUES (#{noteTitle},#{noteDescription},#{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
