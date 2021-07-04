e8016f04fc122ade06705971aac1175cca5dce1a
sabrine-gamdou <sgamdou@stud.hs-bremen.de>
2021-07-04 17:04:15 +0200

    Added feedback messages constants file to be reused.

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/utils/FeedbackMessages.java
@@ -0,0 +1,41 @@
+package com.udacity.jwdnd.course1.cloudstorage.utils;
+
+public final class FeedbackMessages {
+
+    /* ----FILES---- */
+    public static final String FILE_SHOULD_NOT_BE_EMPTY = "File should not be empty!";
+    public static final String FILE_NAME_ALREADY_USED = "File name already used";
+    public static final String FILE_SIZE_LIMIT_EXCEEDED = "File size limit exceeded";
+    public static final String FILE_DELETION_ERROR = "There was an error deleting the file, please try again.";
+
+    public static final String FILE_CREATED_SUCCESSFULLY = "File created successfully!";
+    public static final String FILE_DELETED_SUCCESSFULLY= "File deleted successfully!";
+
+
+    /* ----CREDENTIALS---- */
+    public static final String CREDENTIAL_USERNAME_USED = "The username entered is already used, please use a different one." ;
+    public static final String CREDENTIAL_UPDATE_ERROR = "There was an error updating the credential, please try again.";
+    public static final String CREDENTIAL_CREATION_ERROR = "There was an error creating the credential, please try again.";
+    public static final String CREDENTIAL_DELETION_ERROR = "There was an error deleting the credential, please try again.";
+
+    public static final String CREDENTIAL_CREATED_SUCCESSFULLY = "Credential created successfully!";
+    public static final String CREDENTIAL_UPDATED_SUCCESSFULLY = "Credential updated successfully!";
+    public static final String CREDENTIAL_DELETED_SUCCESSFULLY = "Credential deleted successfully!";
+
+    /* ----NOTES---- */
+    public static final String NOTE_DESCRIPTION_LENGTH_LIMIT_EXCEEDED = "The description text must have less than 1000 characters!";
+    public static final String NOTE_TITLE_USED = "The note title is already used!";
+    public static final String NOTE_CREATION_ERROR = "There was an error creating the note, please try again.";
+    public static final String NOTE_UPDATE_ERROR = "There was an error for updating a note. Please try again";
+    public static final String NOTE_DELETION_ERROR = "There was an error deleting a note. Please try again";
+
+    public static final String NOTE_CREATED_SUCCESSFULLY = "Note created successfully!";
+    public static final String NOTE_UPDATED_SUCCESSFULLY = "Note updated successfully!";
+    public static final String NOTE_DELETED_SUCCESSFULLY = "Note deleted successfully!";
+
+    /* ----USER SIGNUP---- */
+    public static final String USERNAME_USED = "The username already exists.";
+    public static final String USER_SIGNUP_ERROR = "There was an error signing you up, please try again.";
+    public static final String USER_SIGNED_UP_SUCCESSFULLY = "You successfully signed up!";
+
+}
